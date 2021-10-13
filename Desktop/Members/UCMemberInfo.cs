6ca2534d88fd5edb7b319ecfca5b4771e675548a
Minh Tiến Đặng <tiendvlp@gmail.com>
2021-10-13 10:36:23 +0700

    'update'

--- Desktop/Members/UCMemberInfo.cs
+++ Desktop/Members/UCMemberInfo.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessObject;
+using System;
 using System.Windows.Forms;
 
 namespace Desktop.Members
@@ -14,7 +8,75 @@ namespace Desktop.Members
     {
         public UCMemberInfo()
         {
-            InitializeComponent();
+           InitializeComponent();
+        }
+
+
+        public (string name, string email, string password, string city, string country, string companyName)? GetUserInput()
+        {
+            if (String.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("User name can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+            if (String.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("User email can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+            if (String.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("User password can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+            if (String.IsNullOrWhiteSpace(txtCity.Text))
+            {
+                MessageBox.Show("User city can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+            if (String.IsNullOrWhiteSpace(txtCountry.Text))
+            {
+                MessageBox.Show("User country can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+            if (String.IsNullOrWhiteSpace(txtCompanyName.Text))
+            {
+                MessageBox.Show("User company name can not be empty", "Missing info", MessageBoxButtons.OK);
+                return null;
+            }
+
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            string city = txtCity.Text;
+            string country = txtCountry.Text;
+            string companyName = txtCompanyName.Text;
+
+            return (
+                    name: name, email: email, password: password, city: city, country: country
+                    , companyName: companyName
+                );
+        }
+
+        internal void ClearInput()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            txtPassword.Clear();
+            txtCity.Clear();
+            txtCountry.Clear();
+            txtCompanyName.Clear();
+        }
+
+
+        internal void SetContent(Member member)
+        {
+            txtName.Text = member.Name;
+            txtEmail.Text = member.Email;
+            txtPassword.Text = member.Password;
+            txtCity.Text = member.City;
+            txtCountry.Text = member.Country;
+            txtCompanyName.Text = member.CompanyName;
         }
     }
 }
